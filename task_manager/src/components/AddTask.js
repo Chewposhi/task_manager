4eebaee0f2a8817db8b889463e2d8bf39c36ba43
Chew Poshi <chewposhi@gmail.com>
2024-03-29 15:13:02 +0800

    Update AddTask.js

--- task_manager/src/components/AddTask.js
+++ task_manager/src/components/AddTask.js
@@ -112,7 +112,7 @@ const AddTask = ({ todo, isEdit }) => {
           className="shadow appearance-none border rounded w-full py-2 px-3 text-white-700 leading-tight focus:outline-none focus:shadow-outline"
         />
       </div>
-      <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">{isEdit? "Save" :"Submit"}</button>
+      <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">{isEdit? todo.status=="Completed" ? "Uncomplete Task" : "Save" :"Submit"}</button>
     </form>
   );
 };
